bb50b9528444128b1dbc202b9b1250782d3bfd31
Viktor Zamaruiev <vzamaruiev@cogniance.com>
2016-05-04 12:51:58 +0300

    Add writing to file

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var casper = require('casper').create();
 var utils = require('utils');
-entity = JSON.parse(casper.cli.get('entity'));
-
 var system = require('system');
-entity.id = "dockerId";
-system.stdout.writeLine(JSON.stringify(entity));
+
+var entity = JSON.parse(casper.cli.get('entity'));
+
+var result = new Object();
+result.scraperId = guid();
+result.entity = entity;
+result.entity.id = Math.floor(Math.random() * 1000) + 1;
+
+system.stdout.writeLine(JSON.stringify(result));
 
 casper.exit();
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
